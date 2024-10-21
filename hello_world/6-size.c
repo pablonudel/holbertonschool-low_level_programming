09524a8ccba74bc5655a100c3c5031d5622ff496
pablonudel <pablonudel@gmail.com>
2024-10-21 12:26:31 +0200

    6.Size is not grandeur...

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints size of various types
+ * Return: 0
+ */
+int main(void)
+{
+	char CHAR;
+	int INT;
+	long int LINT;
+	long long int LLINT;
+	float FLOAT;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(CHAR));
+	printf("Size of an int: %lu byte(s)\n", sizeof(INT));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(LINT));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(LLINT));
+	printf("Size of a float: %lu byte(s)", sizeof(FLOAT));
+	return (0);
+}
