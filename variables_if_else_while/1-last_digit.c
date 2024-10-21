e668d737890a162c22a8c633470d299e790b66ee
pablonudel <pablonudel@gmail.com>
2024-10-21 15:57:27 +0200

    1.the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Last digit of %d is %d ", n, lastdigit);
 	if (lastdigit > 5)
 	{
-		printf("and is grater than 5\n");
+		printf("and is greater than 5\n");
 	}
 	else if (lastdigit < 6 && lastdigit != 0)
 	{
