5d52a424fddd8a3ab3b5078ceba482098b30ab9b
pablonudel <pablonudel@gmail.com>
2024-10-22 16:01:53 +0200

    0._putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - prints _putchar followed by a new line
@@ -6,6 +5,14 @@
  */
 int main(void)
 {
-	_putchar();
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
 	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef main_h
 #define main_h
 
-void _putchar(char c);
+int _putchar(char c);
 #endif
