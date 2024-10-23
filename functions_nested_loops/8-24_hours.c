d29f462fa55cfd352640a37f7257a937deb69b6a
pablonudel <pablonudel@gmail.com>
2024-10-23 16:20:19 +0200

    8.24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,51 +6,5 @@
  */
 void jack_bauer(void)
 {
-	int h1;
-	int h2;
-	int m1;
-	int m2;
 
-	h1 = 48;
-	h2 = 48;
-	m1 = 48;
-	m2 = 48;
-
-	_putchar(h1);
-	_putchar(h2);
-	_putchar(58);
-	_putchar(m1);
-	_putchar(m2);
-	_putchar('\n');
-	
-	while (h1 < 51 && h2 < 52 && m1 < 54 && m2 < 58)
-	{
-		m2++;
-
-		if (m2 == 57)
-		{
-			m2 = 48;
-			m1++;
-		}
-		if (m1 == 53 && m2 == 57)
-		{
-			h2++;
-			m1 = 48;
-			m2 = 48;
-		}
-		if (h2 == 57 && m1 == 53 && m2 == 57)
-		{
-			h1++;
-			h2 = 48;
-			m1 = 48;
-			m2 = 48;
-		}
-
-		_putchar(h1);
-        	_putchar(h2);
-        	_putchar(58);
-        	_putchar(m1);
-        	_putchar(m2);
-        	_putchar('\n');
-	}
 }
