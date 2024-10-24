44925314b14162804de5f6b044b1faad04ac1704
pablonudel <pablonudel@gmail.com>
2024-10-24 17:55:24 +0200

    9.times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,28 +13,27 @@ void times_table(void)
 		for (num2 = 0; num2 < 10; num2++)
 		{
 			r = num1 * num2;
-                	ld = r % 10;
-                	fd = (r - ld) / 10;
-			
-			if (r > 9)
+			ld = r % 10;
+			fd = (r - ld) / 10;
+
+			if (num2 != 0)
 			{
-				_putchar('0' + fd);
-			}
-			
-			_putchar('0' + ld);
-			_putchar(',');
-			
-			if (r < 10)
-			{
+				_putchar(',');
 				_putchar(' ');
+
+				if (r >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + fd);
+				}
 			}
-			
-			_putchar(' ');
-			
+
+			_putchar('0' + ld);
+
 			if (num2 == 9)
 			{
 				_putchar('\n');
-			}	
+			}
 		}
 	}
 }
