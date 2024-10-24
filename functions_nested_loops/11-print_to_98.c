3c0ba50265f31f68bdd900b03c0194f1ff82f2b7
pablonudel <pablonudel@gmail.com>
2024-10-24 21:08:35 +0200

    11.print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -16,9 +16,9 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	if (n > 97 && n != 98)
+	if (n > 98 && n != 98)
 	{
-		for (; n > 97; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
