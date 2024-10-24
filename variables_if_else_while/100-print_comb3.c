17978d4d30ec387a471372d89102fd037be402fe
pablonudel <pablonudel@gmail.com>
2024-10-24 21:35:52 +0200

    100.print comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * return: 0
+ */
+int main(void)
+{
+	int i, a;
+
+	for (i = 48; i < 58; i++)
+	{
+		
+
+	return (0);
+}
