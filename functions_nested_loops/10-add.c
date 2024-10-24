f1a1433188063a5cd26a7b9281acd2c847d6e6ba
pablonudel <pablonudel@gmail.com>
2024-10-24 19:02:20 +0200

    10.a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: an int as result.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef main_h
 #define main_h
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +10,5 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 #endif
