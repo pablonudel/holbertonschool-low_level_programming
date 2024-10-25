b64fdf110495e8d592b75d0c0b8931af2bcd7d42
pablonudel <pablonudel@gmail.com>
2024-10-25 13:54:58 +0200

    8.24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,27 +7,18 @@
 void jack_bauer(void)
 {
 	int hh;
-	int hfd;
-	int hld;
 	int mm;
-	int mfd;
-	int mld;
 
 	hh = 0;
 	mm = 0;
 
 	while (hh < 24 && mm < 60)
 	{
-		mld = mm % 10;
-		mfd = (mm - mld) / 10;
-		hld = hh % 10;
-		hfd = (hh - hld) / 10;
-
-		_putchar('0' + hfd);
-		_putchar('0' + hld);
+		_putchar('0' + (hh / 10));
+		_putchar('0' + (hh % 10));
 		_putchar(':');
-		_putchar('0' + mfd);
-		_putchar('0' + mld);
+		_putchar('0' + (mm / 10));
+		_putchar('0' + (mm % 10));
 		_putchar('\n');
 
 		if (mm == 59)
