c10d6968fe8fc2ca0b2d4494e61edf574788b918
pablonudel <pablonudel@gmail.com>
2024-10-25 16:09:19 +0200

    100.times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,9 +1,51 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line.
- * @n: first printed number.
+ * print_times_table - prints the n times table, starting with 0.
+ * If n is greater than 15 or less than 0 the function should not print anything
+ * @n: n tables.
  *
  * Return: void
  */
+void print_times_table(int n)
+{
+	int l, c, r;
+
+	if (n > -1 && n < 16)
+	{
+		for (l = 0; l < n + 1; l++)
+		{
+			for (c = 0; c < n + 1; c++)
+			{
+				r = l * c;
+				if (c != 0)
+				{
+					_putchar(',');
+					if (r / 100 && r > 99)
+					{
+						_putchar(' ');
+						_putchar('0' + (r / 100));
+						_putchar('0' + (r / 10));
+						_putchar('0' + (r % 10));
+					}
+					else if (r / 10 && r < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + (r / 10));
+						_putchar('0' + (r % 10));
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + (r % 10));
+					}
+				}
+				_putchar('0' + (r % 10));
+			}
+			_putchar('\n');
+		}
+	}
+}
