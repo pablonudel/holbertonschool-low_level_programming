421c0ad4e9fc0306265045757811ebfcb96875c9
pablonudel <pablonudel@gmail.com>
2024-10-25 09:18:01 +0200

    100.print comb3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,11 +6,29 @@
  */
 int main(void)
 {
-	int i, a;
+	int fstDig, lstDig;
 
-	for (i = 48; i < 58; i++)
+	fstDig = 0;
+	lstDig = fstDig + 1;
+
+	for (fstDig = 0; fstDig < lstDig ; fstDig++)
 	{
-		
+		for (lstDig = fstDig + 1; lstDig < 10; lstDig++)
+		{
+			putchar('0' + fstDig);
+			putchar('0' + lstDig);
+
+			if (fstDig == 8)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
 	return (0);
 }
