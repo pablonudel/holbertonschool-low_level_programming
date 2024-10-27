e1ca493750cd28d1d79e03ffa007c3ef38389a5b
pablonudel <pablonudel@gmail.com>
2024-10-27 10:18:53 +0100

    101.natural

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -7,41 +7,19 @@
  */
 int main(void)
 {
-	int n, r1, r2, r3, total;
+	int n, total;
 
-	r1 = 0;
-	r2 = 0;
-	r3 = 0;
+	n = 0;
+	total = 0;
 
-	for (n = 0; r1 < 1024; n++)
+	for (n = 0; n * 3 < 1024; n++)
 	{
-		if (((n * 3) / 5) % 3 != 0)
+		total += n * 3;
+		if ((n * 5) % 3 != 0 && n * 5 < 1024)
 		{
-			r1 = n * 3;
-			total += r1;
+			total += n * 5;
 		}
 	}
-	
-	for (n = 0; r2 < 1024; n++)
-	{
-		if (((n * 5) / 3) % 5 != 0)
-		{
-			r2 = n * 5;
-			total += r2;
-		}
-	}
-
-	for (n = 0; r3 < 1024; n++)
-	{
-		if (((n * 5) / 3) % 5 == 0 && ((n * 3) / 5) % 3 == 0)
-		{
-			r3 = n * 3;
-			total += r3;
-			r3 = n * 5;
-			total += r3;
-		}
-	}
-
 	printf("%d\n", total);
 
 	return (0);
