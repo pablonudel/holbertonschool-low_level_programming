51baf6fa1fd1a2c16e75cd76b0a3dc866ebbf7ff
pablonudel <pablonudel@gmail.com>
2024-10-29 10:03:41 +0100

    3.puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -16,10 +16,13 @@ void _puts(char *str)
 		length = s - str;
 	}
 
-	for (i = 0; i < length + 1; i++)
-		{
-			_putchar(str[i]);
-		}
+	if (length != 0)
+	{
+		for (i = 0; i < length + 1; i++)
+			{
+				_putchar(str[i]);
+			}
+	}
 	if (i == length + 1 || length == 0)
 		_putchar('\n');
 }
