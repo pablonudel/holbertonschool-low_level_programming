ad27476830ec9e7fe8307d84126c7b12bc95925b
pablonudel <pablonudel@gmail.com>
2024-10-31 15:17:26 +0100

    3.strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,25 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0, s1_len = 0, s2_len = 0;
+	int i = 0;
 
-	while (s1[s1_len] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		s1_len++;
+		i++;
 	}
 
-	while (s2[s2_len] != '\0')
-	{
-		s2_len++;
-	}
-
-	if (s1_len == s2_len)
-		res = 0;
-	else
-		res = s1_len + s2_len + 4;
-
-	if (s1_len < s2_len)
-		res = res * -1;
-
-	return (res);
+	return (s1[i] - s2[i]);
 }
