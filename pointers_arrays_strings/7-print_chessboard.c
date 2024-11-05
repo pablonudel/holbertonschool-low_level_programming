f69848a77d263e64df9a129c5f28193076d99957
pablonudel <pablonudel@gmail.com>
2024-11-05 09:38:25 +0100

    7.print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: char
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[i][x]);
+		}
+		_putchar('\n');
+	}
+}
