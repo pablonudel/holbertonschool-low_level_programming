41477ac579973deaa04b5891da8ea28a4bd9eb56
pablonudel <pablonudel@gmail.com>
2024-11-06 16:37:45 +0100

    5.sqrt_recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
  */
 int find_sqrt(int n, int i)
 {
-	if (i * i > n || n < 0)
+	if (i * i > n)
 		return (-1);
 
 	if (i * i == n)
@@ -24,5 +24,8 @@ int find_sqrt(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
+
 	return (find_sqrt(n, 0));
 }
