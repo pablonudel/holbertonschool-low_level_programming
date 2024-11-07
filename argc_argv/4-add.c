048e0ae28b969129280d0e4221567ec74c41f237
pablonudel <pablonudel@gmail.com>
2024-11-07 15:04:26 +0100

    4.add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - adds positive numbers.
  * @argc: int, arguments supplied to the program
@@ -9,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	unsigned int res = 0;
 
 	if (argc < 2)
@@ -20,14 +21,16 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
-		if (atoi(argv[i]) > 0)
-			res += atoi(argv[i]);
+		res += atoi(argv[i]);
 
 	}
 	printf("%u\n", res);
