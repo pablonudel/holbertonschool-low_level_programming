dc7088cb3263870a410da5d2088a0a8851e80eb9
pablonudel <pablonudel@gmail.com>
2024-11-07 13:57:47 +0100

    4.add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: int, arguments supplied to the program
+ * @argv: char, array of pointers to the strings of the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, res = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		res += atoi(argv[i]);
+
+	}
+	printf("%d\n", res);
+	return (0);
+}
