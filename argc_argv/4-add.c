d6a7a736b105c843c8c387f9fb4925378fa33eac
pablonudel <pablonudel@gmail.com>
2024-11-07 14:45:35 +0100

    4.add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,13 +19,14 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) <= 0)
+		if (atoi(argv[i]) == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
 
-		res += atoi(argv[i]);
+		if (atoi(argv[i]) > 0)
+			res += atoi(argv[i]);
 
 	}
 	printf("%d\n", res);
