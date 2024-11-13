52cc5fa6b4701423e08c158d721fa44c65164ccb
pablonudel <pablonudel@gmail.com>
2024-11-13 15:49:47 +0100

    struct doc

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - pet info
+ * @name: char pointer - Name of dog
+ * @age: float - age of dog
+ * @owner: char pointer - name of owner
+ *
+ * Description: pet information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
