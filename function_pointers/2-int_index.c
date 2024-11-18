8c6d3fff79fd93bea89d427f808b5b4b2d5be7f6
pablonudel <pablonudel@gmail.com>
2024-11-18 16:03:04 +0100

    3.calc

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,14 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL || array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
-		{
 			return (i);
-		}
 
 	return (-1);
 
