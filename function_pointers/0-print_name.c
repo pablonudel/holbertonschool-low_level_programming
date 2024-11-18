32d2709049913e90e3bd1c2e0c7afaad694d8991
pablonudel <pablonudel@gmail.com>
2024-11-18 10:14:37 +0100

    0-print_name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - pass a name to a function to print it
+ * @name: char pointer - name
+ * @f: function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
