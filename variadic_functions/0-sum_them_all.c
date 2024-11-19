6c8a3e4512b5f6f78ecdc545c40e45bea6972966
pablonudel <pablonudel@gmail.com>
2024-11-19 20:53:17 +0100

    rm condition

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * sum_them_all - sum of all its parameters.
  * @n: unsigned int for count
@@ -12,13 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	va_list args;
 
-	if (n > 0)
-	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-			res += va_arg(args, int);
-		va_end(args);
-	}
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		res += va_arg(args, int);
+	va_end(args);
+
 
 	return (res);
 }
