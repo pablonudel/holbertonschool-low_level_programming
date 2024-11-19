ed20d1c06e2ef3329a8df51bdbb7d119a671b1f5
pablonudel <pablonudel@gmail.com>
2024-11-19 20:37:33 +0100

    adding conditional

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,14 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	va_list args;
 
-	va_start(args, n);
-
 	if (n > 0)
+	{
+		va_start(args, n);
 		for (i = 0; i < n; i++)
 			res += va_arg(args, int);
-
-	va_end(args);
-
+		va_end(args);
+	}
 
 	return (res);
 }
