24c57e5c71f125b991a3c3837590dd01841764f8
pablonudel <pablonudel@gmail.com>
2024-11-19 20:27:09 +0100

    fixing sintaxis - removing conditional

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,15 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	va_list args;
 
-	if (n > 0)
-	{
-		va_start(args, n);
+	va_start(args, n);
 
-		for (i = 0; i < n; i++)
-			res += va_arg(args, int);
+	for (i = 0; i < n; i++)
+		res += va_arg(args, int);
 
-		va_end(args);
-	}
+	va_end(args);
+
 
 	return (res);
 }
