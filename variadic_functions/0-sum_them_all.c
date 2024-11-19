0784c408789112a8ea3b9709b3bc70cd0ab6c56b
pablonudel <pablonudel@gmail.com>
2024-11-19 21:11:01 +0100

    fix type in va_arg

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,9 +15,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-		res += va_arg(args, int);
+		res += va_arg(args, unsigned int);
 	va_end(args);
-
 
 	return (res);
 }
