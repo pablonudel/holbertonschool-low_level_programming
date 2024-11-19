a2224e2369aaaf728d3b0565d51241b0b29f5d96
pablonudel <pablonudel@gmail.com>
2024-11-19 18:57:15 +0100

    fix sintaxis of condition

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,15 +12,16 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	va_list args;
 
-	if (n == NULL || n == 0)
-		return (0);
+	if (n != 0)
+	{
+		va_start(args, n);
 
-	va_start(args, n);
+		for (i = 0; i < n; i++)
+			res += va_arg(args, int);
 
-	for (i = 0; i < n; i++)
-		res += va_arg(args, int);
+		va_end(args);
+		return (res);
+	}
 
-	va_end(args);
-
-	return (res);
+	return (0);
 }
