4baac250b29d09864ebae94ccc29cca873d418d3
pablonudel <pablonudel@gmail.com>
2024-11-19 10:04:22 +0100

    0.sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sum of all its parameters.
+ * @n: unsigned int for count
+ * @...: multiples args
+ *
+ * Return: int - result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int res = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		res += va_arg(args, unsigned int);
+	}
+	va_end(args);
+
+	return (res);
+}
