e2b57f26b9d965bcc8985099c35c1e74ee9d8923
pablonudel <pablonudel@gmail.com>
2024-11-20 10:07:57 +0100

    0.sum_them_all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,21 +3,21 @@
  * sum_them_all - Sum all element
  * @n: the number of element to sum
  *
- * Return: 0 if n == 0 else the sum of elem
+ * Return: res or 0 if n is equal to 0.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int s = 0;
+	int res = 0;
 	unsigned int i;
 	va_list args;
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, 0);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
-		s += va_arg(args, int);
+		res += va_arg(args, int);
 	va_end(args);
 
-	return (s);
+	return (res);
 }
