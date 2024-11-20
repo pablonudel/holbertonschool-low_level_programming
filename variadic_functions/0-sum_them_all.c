c579023505b772d27aa49437a3a91d44bc8b197e
pablonudel <pablonudel@gmail.com>
2024-11-20 08:57:54 +0100

    fix sintaxis

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,13 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	va_list args;
 
-	if (n > 0)
-	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
-			res += va_arg(args, int);
-		va_end(args);
-	}
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		res += va_arg(args, int);
+
+	va_end(args);
 
 	return (res);
 }
