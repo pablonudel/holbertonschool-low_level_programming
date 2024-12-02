f04fbea290740337d3c814869c824378686cb44f
pablonudel <pablonudel@gmail.com>
2024-12-02 13:09:43 +0100

    adding conditional for if there is not list_t current

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,12 +3,16 @@
  * print_list - prints all the elements of a list_t list.
  * @h: list_h pointer to a string
  *
- * Return: size_t - Return: the number of nodes
+ * Return: size_t - the number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
 	unsigned int i = 0;
+
+	if (!current)
+		printf("[0] (nil)\n");
+		return (1);
 
 	while (current != NULL)
 	{
