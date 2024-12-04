f18199cb1991ef574830076a473625c1be4d0a73
pablonudel <pablonudel@gmail.com>
2024-12-04 15:00:55 +0100

    fixing missed prev_node->prev declaration at the end

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 
 	prev_node = *h;
-	while (idx != 1 && i < idx - 1)
+	while (i < idx - 1)
 	{
 		if (!prev_node)
 			return (NULL);
@@ -33,8 +33,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = prev_node;
 	new_node->next = prev_node->next;
+	new_node->prev = prev_node;
+	prev_node->next->prev = new_node;
 	prev_node->next = new_node;
 	return (new_node);
 }
