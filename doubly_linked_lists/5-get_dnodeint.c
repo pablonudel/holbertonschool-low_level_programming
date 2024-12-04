3589af226e81ac2e749e9969fe250d8155470dfa
pablonudel <pablonudel@gmail.com>
2024-12-04 14:03:14 +0100

    fix trailing whitespace

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (NULL);
 
 	while (i < index)
-	{	
+	{
 		if (!current)
 			return (NULL);
 		current = current->next;
