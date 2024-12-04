11727e7ddd397adae8ffe4471f30f6d64d3bfe79
pablonudel <pablonudel@gmail.com>
2024-12-04 14:08:40 +0100

    removing arguments conditional

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -12,9 +12,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	unsigned int i = 0;
 
-	if (!head || !index)
-		return (NULL);
-
 	while (i < index)
 	{
 		if (!current)
