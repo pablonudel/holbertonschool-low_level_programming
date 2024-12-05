2a1710cbce57679d29bfa35367dc8e1fa86ae3e0
pablonudel <pablonudel@gmail.com>
2024-12-05 13:28:35 +0100

    filling buffer with text_content

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,13 +9,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fw;
+	int fd, fw, i = 0;
 	char *buffer;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -30,7 +30,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fw = write(fd, buffer, sizeof(text_content));
+	while (text_content[i])
+	{
+		buffer[i] = text_content[i];
+		i++;
+	}
+	buffer[i] = '\0';
+
+	fw = write(fd, buffer, sizeof(buffer));
 	if (fw == -1)
 	{
 		close(fd);
