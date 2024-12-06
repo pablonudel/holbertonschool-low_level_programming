805681f7a371bfec4da957acc4125655cdc2e9b2
pablonudel <pablonudel@gmail.com>
2024-12-07 00:16:41 +0100

    fix syntaxis

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,22 +3,22 @@
  * f_error - print error msg on the POSIX standard error and exit
  * @fd_from: return of open/rean file_from
  * @fd_to: return of open/rean file_to
- * @av: command arguments
+ * @argv: command arguments
  * @buffer: buffer
  *
  * Return: void
  */
-void f_error(int fd_from, int fd_to, char **av, char *buffer)
+void f_error(int fd_from, int fd_to, char *argv[], char *buffer)
 {
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
 		exit(98);
 	}
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(buffer);
 		exit(99);
 	}
@@ -40,41 +40,41 @@ void c_error(int fc, int fd)
 }
 /**
  * main - copies the content of a file to another file
- * @ac: number of args
- * @av: array with the args
+ * @argc: number of args
+ * @argv: array with the args
  *
  * Return: 0
  */
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, fc, fr, fw;
 	char *buffer;
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
+	fd_from = open(argv[1], O_RDONLY);
 	buffer = malloc(sizeof(char) * 1024);
 	if (!buffer)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	fr = read(fd_from, buffer, 1024);
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	f_error(fd_from, fd_to, av, buffer);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	f_error(fd_from, fd_to, argv, buffer);
 
 	do {
 		if (fr == -1)
-			f_error(fr, 0, av, buffer);
+			f_error(fr, 0, argv, buffer);
 		fw = write(fd_to, buffer, fr);
 		if (fw == -1)
-			f_error(0, fw, av, buffer);
+			f_error(0, fw, argv, buffer);
 		fr = read(fd_from, buffer, 1024);
-		fd_to = open(av[2], O_WRONLY | O_APPEND);
+		fd_to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (fr > 0);
 
 	free(buffer);
